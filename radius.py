45d378f27b2f81d79f2e0097c6eb9b77879ed861
Skibidy Toilet <dube.kshitiz@gmail.com>
2024-12-23 19:25:52 +0200

    hello

--- radius.py
+++ radius.py
@@ -1,3 +1,7 @@
-radius = int(input("what is your radius of your circle "))
-area = 22/7*(radius**2)
-print("The area of this circle is ",area)
+import math    
+try:
+    radius = int(input("what is your radius of your circle "))
+    area = math.pi*(radius**2)
+    print("The area of this circle is ",area)
+except:
+    print("The radius can only be a whole number")
