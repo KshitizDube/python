d60a546a1ecade82e6e67644726133913cefbafb
Skibidy Toilet <dube.kshitiz@gmail.com>
2024-12-25 11:09:06 +0200

    i learned how to ubduct b

---
+++ ubduct_b.py
@@ -0,0 +1,11 @@
+listt_from_user = int(input("How many words do you want to enter : "))
+second_list_of_words = [] 
+third_list = []
+for i in range(listt_from_user):
+    word_user_gave = input("please enter your word : ")
+    second_list_of_words.append(word_user_gave)
+print(second_list_of_words)
+for p in second_list_of_words:
+    if "b" not in p:
+        third_list.append(p)
+print(f"Thi is the lists with no words containing b, {third_list}")
